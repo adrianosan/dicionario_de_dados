6d50678045c9b9ae619462ec5684cc90fcf929fa
adriano.santos <adriano.santos@squadra.com.br>
2019-06-12 19:05:14 -0300

    dicionario de dados

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, int> dc = new Dictionary<string, int>()
+            {
+
+                { "abc",1 },
+                { "bbc" ,2 },
+                { "cbc" ,3 },
+                {"ebc", 4 }
+                };
+
+            foreach(KeyValuePair<string,int>pair in dc)
+            {
+                Console.WriteLine("{0},{1}", pair.Key, pair.Value);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
